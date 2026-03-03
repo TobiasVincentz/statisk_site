f961c594232793a35a49398e1fe19d33a969998b
Tobias <tovi0001@stud.ek.dk>
2026-03-03 12:32:38 +0100

    details fixet

---
+++ js/productdetails.js
@@ -0,0 +1,42 @@
+const id = new URLSearchParams(window.location.search).get("id");
+const endpoint = `https://kea-alt-del.dk/t7/api/products/${id}`;
+// console.log(endpoint);
+const container = document.querySelector(".product-page");
+
+function getData() {
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then(showData);
+}
+
+function showData(json) {
+  //   console.log(id);
+  //   console.log(endpoint);
+  container.innerHTML = `
+        <div class="product-image">
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/640/${json.id}.webp"
+            alt="Produkt billede"
+          />
+        </div>
+        <div class="product-info">
+          <h1 class="h1_font">${json.productdisplayname}</h1>
+          <p class="price">Brand: ${json.brandname}</p>
+          <p class="price">Color: ${json.colour1}</p>
+          <p class="price inventory">Item no: ${json.id}</p>
+          <p class="price">DKK ${json.price},-</p>
+          <form class="size-form">
+            <label for="size" class="price inventory">SIZE</label>
+            <select id="size" name="size">
+              <option value="">Select size</option>
+              <option>S</option>
+              <option>M</option>
+              <option>L</option>
+              <option>XL</option>
+            </select>
+            <button class="add-to-basket price">Add to basket</button>
+          </form>
+        </div>`;
+}
+
+getData();
--- js/produktliste.js
+++ js/produktliste.js
@@ -3,7 +3,7 @@ const klikCategory = new URLSearchParams(window.location.search).get(
 );
 // console.log(klikCategory);
 const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${klikCategory}`;
-console.log(endpoint);
+// console.log(endpoint);
 const container = document.querySelector(".product-list");
 
 function getData() {
@@ -16,15 +16,15 @@ function showData(json) {
   // console.log(json);
   let markup = "";
   json.forEach((product) => {
-    markup += `<a href="produkt.html">
+    markup += `<a href="productdetails.html?id=${product.id}">
     <article class="smallProduct onSale soldOut">
     <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product image" />
     <h3 class="product">${product.productdisplayname}</h3>
     <p class="categorycard">Tshirts Nike</p>
-    <p class="price">DKK <span>${product.price}</span>,-</p> 
+    <p class="beforeprice">DKK <span>${product.price}</span>,-</p> 
     <div class="discounted">
     <p class="price">Now DKK <span class="newprice">974</span>,-</p>
-    <p><span>${product.discount}</span>%</p>
+    <p class="discount"><span>${product.discount}</span>%</p>
     </div>
     </article>
     </a>
